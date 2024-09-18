fb6b2db53ee8dddc0bc292b9909fe6dc93cad054
HimanshuPatil2003 <ram03patil@gmail.com>
2024-09-18 10:07:47 +0530

    password bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
     email:{
@@ -13,5 +14,26 @@ const userSchema = new Schema({
     }
 
 })
+
+//static signup method
+userSchema.statics.signup = async function(email, password){
+    //email checking if exisit
+    const exists=await this.findOne({email})
+    if(exists){
+        throw Error('Email already in use')
+    }
+
+    //first hash the password using bcrypt
+    //it adds random string to current pass before hashing
+
+    const salt = await bcrypt.genSalt(10/*no of rounds of hash*/)
+    const hash = await bcrypt.hash(password, salt) // bcrypt hash password in this line 
+
+
+    const user = await this.create({email, password:hash})
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
